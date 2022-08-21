ecc0f08fb681bc227a733ced3f94117d6d22f387
jamshimfinu <111169920+jamshimfinu@users.noreply.github.com>
2022-08-21 15:44:28 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,12 +243,12 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton(" join now ", url="https://t.me/MovieCloudAI"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("BOT Channel", url="https://t.me/MovieCloudAI"),
+                    InlineKeyboardButton("Support Group", url="https://t.me/MovieCloudAI")
                 ]
             ]
         ),
@@ -265,7 +265,7 @@ async def about(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://github.com/jamshimfinu/Unlimited-Filter-Max-Bot")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
